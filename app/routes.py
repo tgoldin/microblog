25ef8b95b70f111f6246d1242809e02aeaca311b
tgoldin <tpg@blueyonder.co.uk>
2020-06-29 16:46:17 +0100

    Updated index.html with test posts

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Tony'}
-    return render_template('index.html', title='Home',user=user)
+    posts = [
+        {
+            'author': {'username': 'T800'},
+            'body': '''I'm not obsolete!'''
+        },
+        {
+            'author': {'username': 'T1000'},
+            'body': '''I'll be back!'''
+        },
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
